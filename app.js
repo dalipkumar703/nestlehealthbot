66171d5ecad283925aed889c29937ca2bde4eff0
dalip123 <dalipkumar703@gmail.com>
2017-06-20 18:31:39 +0530

    update master

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ mainController(app);
 //test(app);
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost: " + app.get('port'))
 })
 console.log("hello app");
-
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -928,7 +928,7 @@ module.exports = function(app) {
       //
       // You must send back a 200, within 20 seconds, to let us know
       // you've successfully received the callback. Otherwise, the request
-      // will time out and we will keep trying to resend.
+      // will time out and we will keep trying to resend. hello
       res.sendStatus(200);
     }
   });
