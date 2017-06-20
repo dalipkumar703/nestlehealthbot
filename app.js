84c04e20c93e55caf0a8581aadcc26c827570cb6
dalip <dalipkumar703@gmail.com>
2017-06-20 18:07:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var app = express();
 var dotenv = require('dotenv');
 dotenv.load();
 
-
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static('./public'));
 mainController(app);
 //test(app);
 
-app.listen(process.env.PORT||5000);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
 console.log("hello app");
+
