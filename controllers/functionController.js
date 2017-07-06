887f1a8608baaff33175e0e78dc2af2efd6e489a
dalip123 <dalipkumar703@gmail.com>
2017-07-06 15:39:21 +0530

    update

--- controllers/functionController.js
+++ controllers/functionController.js
@@ -177,7 +177,7 @@ exports.callSendAPI = function(messageData) {
         messageId, recipientId);
     } else {
       console.error("Unable to send message.");
-      console.error(response);
+      //console.error(response);
       //console.error(error);
     }
     console.log("hello");
