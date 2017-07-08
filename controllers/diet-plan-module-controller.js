37997d88da80bed8c887bc8599ebc79b0d29dead
dalip123 <dalipkumar703@gmail.com>
2017-07-08 11:50:59 +0530

    check diet

--- controllers/diet-plan-module-controller.js
+++ controllers/diet-plan-module-controller.js
@@ -297,65 +297,35 @@ exports.quickReplyPostback = function(recipient, postback) {
             }
           });
 
-
-   setTimeout(function(){
-
-                 QuickReplyText.find({
-                   $or: [{
-                       payload_for: "VEGAN_DIET"
-                     },
-                     {
-                       payload_for: "VEG_DIET"
-                     },
-                     {
-                       payload_for: "EGG_DIET"
-                     },
-                     {
-                       payload_for: "NON_VEG_DIET"
-                     }
-                   ]
-                 }).exec(function(err, data) {
-                   if (!err) {
+                           QuickReplyText.find({
+                             $or: [{
+                                 payload_for: "VEGAN_DIET"
+                               },
+                               {
+                                 payload_for: "VEG_DIET"
+                               },
+                               {
+                                 payload_for: "EGG_DIET"
+                               },
+                               {
+                                 payload_for: "NON_VEG_DIET"
+                               }
+                             ]
+                           }).exec(function(err, data) {
+                             if (!err) {
 
 
-                         functionController.replyWithPlainText(recipient, data[2].text);
-
-                       QuickReply.find({
-                         $or: [{
-                             payload_for: "VEGAN_DIET"
-                           },
-                           {
-                             payload_for: "VEG_DIET"
-                           },
-                           {
-                             payload_for: "EGG_DIET"
-                           },
-                           {
-                             payload_for: "NON_VEG_DIET"
-                           }
-                         ]
-                       }).exec(function(err, result) {
-                         if (!err) {
-                           console.log("result", result);
-                           for (var i = 0; i < _.size(result); i++) {
-                             title[i] = result[i].title;
-                             payload1[i] = result[i].payload;
-
-                           }
-                           console.log("payload in quick reply:", payload1);
-                           functionController.QuickReplyForTwo(recipient, title, payload1, data[1].text);
-                             console.log("quick reply send");
-                         } else {
-                           console.log("error in reply with url only");
-                         }
-                       });
+                                   functionController.replyWithPlainText(recipient, data[2].text);
 
 
-                   } else {
-                     console.log("error in retrieving from quick reply model");
-                   }
-                 });
-   },9000);
+
+
+                             } else {
+                               console.log("error in retrieving from quick reply model");
+                             }
+                           });
+
+   
           } else {
             console.log("error in getting result from quick reply text ");
           }
